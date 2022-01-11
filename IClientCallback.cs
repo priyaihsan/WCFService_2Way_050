ae777d8f73606305b4fe44911f9722b777ce977c
PriyaIhsan <60973417+priyaihsan@users.noreply.github.com>
2022-01-11 11:38:49 +0700

    Add files via upload

---
+++ IClientCallback.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WCFService_2Way_050
+{
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesenKirim(string user, string pesan);
+    }
+}
